b236186eae1fed226de5e5fed4f86544827096b0
songhm7 <1991203@hansung.ac.kr>
2023-11-06 14:17:29 +0900

    log in and log out

---
+++ app/src/main/java/com/example/mobile/LoginActivity.kt
@@ -0,0 +1,56 @@
+package com.example.mobile
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        findViewById<Button>(R.id.login)?.setOnClickListener {
+            val userEmail = findViewById<EditText>(R.id.username)?.text.toString()
+            val password = findViewById<EditText>(R.id.password)?.text.toString()
+            doLogin(userEmail, password)
+        }
+        findViewById<Button>(R.id.sing_up)?.setOnClickListener {
+            val userEmail = findViewById<EditText>(R.id.username)?.text.toString()
+            val password = findViewById<EditText>(R.id.password)?.text.toString()
+            doSignUp(userEmail, password)
+        }
+    }
+
+    private fun doLogin(userEmail: String, password: String) {
+        Firebase.auth.signInWithEmailAndPassword(userEmail, password)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    startActivity(
+                        Intent(this, MainActivity::class.java))
+                    finish()
+                } else {
+                    Log.w("LoginActivity", "signInWithEmail", it.exception)
+                    Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun doSignUp(userEmail: String, password: String){
+        Firebase.auth.createUserWithEmailAndPassword(userEmail,password)
+            .addOnCompleteListener(this){
+                if(it.isSuccessful){
+                    doLogin(userEmail,password)
+                }
+                else{
+                    Log.w("LoginActivity", "signInWithEmail", it.exception)
+                    Toast.makeText(this, "Sign up failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
--- app/src/main/java/com/example/mobile/MainActivity.kt
+++ app/src/main/java/com/example/mobile/MainActivity.kt
@@ -1,14 +1,34 @@
 package com.example.mobile
 
+import android.content.Intent
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<TextView>(R.id.testTextView).setText("TestText123")
+        if (Firebase.auth.currentUser == null) { //로그인 안돼있으면 로그인액티비티로 전환
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+        findViewById<TextView>(R.id.textUID).text = Firebase.auth.currentUser?.uid ?: "No User" //현재유저id표시
+        findViewById<Button>(R.id.button_signout).setOnClickListener {//로그아웃버튼
+            Firebase.auth.signOut()
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+
     }
 }
