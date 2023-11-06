80953108ad2c78640dc37913585a2a470876b6a7
songhm7 <1991203@hansung.ac.kr>
2023-11-06 13:32:50 +0900

    set testText

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.mobile"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mobile"
--- app/src/main/java/com/example/mobile/MainActivity.kt
+++ app/src/main/java/com/example/mobile/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.mobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.testTextView).setText("TestText")
     }
 }
